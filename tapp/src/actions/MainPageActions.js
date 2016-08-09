01c8255f68106de20782ae3df5a1ad3e8fc47b53
baotang <tlyspa@qq.com>
2016-08-09 16:33:11 +0800

    fix filter

--- tapp/src/actions/MainPageActions.js
+++ tapp/src/actions/MainPageActions.js
@@ -1,13 +1,15 @@
 import * as types from './actionTypes'
 export function mainPageFilterInfo(filter='',dataObj){
     let keyArray = Object.keys(dataObj);
+    let obj = {};
     if(filter){
         filter = filter.toUpperCase();
         keyArray.forEach((key,index)=>{
-            dataObj[key].filter(dataObj=>{
+            obj[key] = dataObj[key].filter(dataObj=>{
                 return dataObj.title.toUpperCase().indexOf(filter) > -1;
             });
         });
+        dataObj = obj;
     }
     return {
         type:types.MAIN_PAGE_FILTER_INFO,
